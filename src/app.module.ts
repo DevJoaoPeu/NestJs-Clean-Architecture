5c5ecd5b8667751ef25d1534f3999412bbbf1cf5
DevJoaoPeu <joaocanaldroid@gmail.com>
2024-03-07 09:17:58 -0300

    feat: Importando modulo forRoot  do typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,18 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProjectsModule } from './projects/projects.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   controllers: [AppController],
   providers: [AppService],
-  imports: [ProjectsModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: ':memory:',
+      synchronize: true,
+    }),
+    ProjectsModule,
+  ],
 })
 export class AppModule {}
