da9b294e091047b27182d47e174a6ba2963ec7bb
DevJoaoPeu <joaocanaldroid@gmail.com>
2024-03-18 09:56:58 -0300

    feat: Criando metodo execute

---
+++ src/projects/use-case/find-all-projects-use-case.ts
@@ -0,0 +1,16 @@
+import { Repository } from 'typeorm';
+import { Project } from '../entities/project.entity';
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+
+@Injectable()
+export class FindALlProjectsUseCase {
+  constructor(
+    @InjectRepository(Project)
+    private readonly projectRepo: Repository<Project>,
+  ) {}
+
+  execute() {
+    return this.projectRepo.find();
+  }
+}
