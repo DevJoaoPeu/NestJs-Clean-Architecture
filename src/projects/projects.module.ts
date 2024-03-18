03bbf6b70ab9fb1e83acd4f1b7041a48e3b7c7e9
DevJoaoPeu <joaocanaldroid@gmail.com>
2024-03-18 09:20:34 -0300

    feat: Alterando importação do controller e passando para o modulo, e adicionando CreateProjectUseCase

--- src/projects/projects.module.ts
+++ src/projects/projects.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ProjectsService } from './projects.service';
-import { ProjectsController } from './projects.controller-with-use-case';
+import { ProjectsControllerWithUseCase } from './projects.controller-with-use-case';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Project } from './entities/project.entity';
+import { CreateProjectUseCase } from './use-case/create-project.use-case';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Project])],
-  controllers: [ProjectsController],
-  providers: [ProjectsService],
+  controllers: [ProjectsControllerWithUseCase],
+  providers: [ProjectsService, CreateProjectUseCase],
 })
 export class ProjectsModule {}
