7c3d13fe0b36b9ad012be132dfb0fa16b89a62d2
DevJoaoPeu <joaocanaldroid@gmail.com>
2024-03-18 09:09:09 -0300

    feat: Regras de negocio do metodo create

---
+++ src/projects/use-case/create-project.use-case.ts
@@ -0,0 +1,18 @@
+import { Repository } from 'typeorm';
+import { CreateProjectDto } from '../dto/create-project.dto';
+import { Project, ProjectStatus } from '../entities/project.entity';
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class CreateProjectUseCase {
+  constructor(private readonly projectRepo: Repository<Project>) {}
+
+  execute(input: CreateProjectDto) {
+    const project = new Project(input);
+    if (input.started_at) {
+      project.status = ProjectStatus.Active;
+    }
+
+    return this.projectRepo.save(project);
+  }
+}
