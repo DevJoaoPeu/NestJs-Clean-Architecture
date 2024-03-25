69c812613bec38b7aebad937b2a02c0db227dc64
DevJoaoPeu <joaocanaldroid@gmail.com>
2024-03-25 09:48:38 -0300

    feat: Injetando dependencia e mudando retorno

--- src/projects/use-case/create-project.use-case.ts
+++ src/projects/use-case/create-project.use-case.ts
@@ -1,22 +1,17 @@
-import { Repository } from 'typeorm';
 import { CreateProjectDto } from '../dto/create-project.dto';
-import { Project, ProjectStatus } from '../entities/project.entity';
-import { Injectable } from '@nestjs/common';
-import { InjectRepository } from '@nestjs/typeorm';
+import { Project } from '../entities/project.entity';
+import { Inject, Injectable } from '@nestjs/common';
+import { IProjectRepository } from '../project.repository';
 
 @Injectable()
 export class CreateProjectUseCase {
   constructor(
-    @InjectRepository(Project)
-    private readonly projectRepo: Repository<Project>,
+    @Inject('IProjectRepository')
+    private readonly projectRepo: IProjectRepository,
   ) {}
 
   execute(input: CreateProjectDto) {
     const project = new Project(input);
-    if (input.started_at) {
-      project.status = ProjectStatus.Active;
-    }
-
-    return this.projectRepo.save(project);
+    return this.projectRepo.create(project);
   }
 }
